4c27ca3f207bce6543a7258fc7538b34db40b89e
Rubendvb <rubenvasquez556@gmail.com>
2024-07-08 03:16:12 -0300

    mobile first

--- src/App.tsx
+++ src/App.tsx
@@ -3,120 +3,249 @@ import './App.css'
 function App() {
   return (
     <>
-      {/* <!-- Item Start --> */}
-      Photosnap New! Featured Senior Frontend Developer 1d ago Full Time USA
-      only
-      {/* <!-- Role --> */}
-      Frontend
-      {/* <!-- Level --> */}
-      Senior
-      {/* <!-- Languages --> */}
-      HTML CSS JavaScript
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      Manage New! Featured Fullstack Developer 1d ago Part Time Remote
-      {/* <!-- Role --> */}
-      Fullstack
-      {/* <!-- Level --> */}
-      Midweight
-      {/* <!-- Languages --> */}
-      Python
-      {/* <!-- Tools --> */}
-      React
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      Account New! Junior Frontend Developer 2d ago Part Time USA only
-      {/* <!-- Role --> */}
-      Frontend
-      {/* <!-- Level --> */}
-      Junior
-      {/* <!-- Languages --> */}
-      JavaScript
-      {/* <!-- Tools --> */}
-      React Sass
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      MyHome Junior Frontend Developer 5d ago Contract USA only
-      {/* <!-- Role --> */}
-      Frontend
-      {/* <!-- Level --> */}
-      Junior
-      {/* <!-- Languages --> */}
-      CSS JavaScript
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      Loop Studios Software Engineer 1w ago Full Time Worldwide
-      {/* <!-- Role --> */}
-      Fullstack
-      {/* <!-- Level --> */}
-      Midweight
-      {/* <!-- Languages --> */}
-      JavaScript Ruby
-      {/* <!-- Tools --> */}
-      Sass
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      FaceIt Junior Backend Developer 2w ago Full Time UK only
-      {/* <!-- Role --> */}
-      Backend
-      {/* <!-- Level --> */}
-      Junior
-      {/* <!-- Languages --> */}
-      Ruby
-      {/* <!-- Tools --> */}
-      RoR
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      Shortly Junior Developer 2w ago Full Time Worldwide
-      {/* <!-- Role --> */}
-      Frontend
-      {/* <!-- Level --> */}
-      Junior
-      {/* <!-- Languages --> */}
-      HTML JavaScript
-      {/* <!-- Tools --> */}
-      Sass
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      Insure Junior Frontend Developer 2w ago Full Time USA only
-      {/* <!-- Role --> */}
-      Frontend
-      {/* <!-- Level --> */}
-      Junior
-      {/* <!-- Languages --> */}
-      JavaScript
-      {/* <!-- Tools --> */}
-      Vue Sass
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      Eyecam Co. Full Stack Engineer 3w ago Full Time Worldwide
-      {/* <!-- Role --> */}
-      Fullstack
-      {/* <!-- Level --> */}
-      Midweight
-      {/* <!-- Languages --> */}
-      JavaScript Python
-      {/* <!-- Tools --> */}
-      Django
-      {/* <!-- Item End --> */}
-      {/* <!-- Item Start --> */}
-      The Air Filter Company Front-end Dev 1mo ago Part Time Worldwide
-      {/* <!-- Role --> */}
-      Frontend
-      {/* <!-- Level --> */}
-      Junior
-      {/* <!-- Languages --> */}
-      JavaScript
-      {/* <!-- Tools --> */}
-      React Sass
-      {/* <!-- Item End --> */}
-      <div className="attribution">
-        Challenge by{' '}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
+      <header></header>
+      <main>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/photosnap.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>Photosnap</span>
+                <div className="status">
+                  <span className="new">New!</span>
+                  <span className="featured">Featured</span>
+                </div>
+              </div>
+              <div>
+                <h2>Senior Frontend Developer</h2>
+              </div>
+              <div className="info">
+                <span>1d ago</span>
+                <span>Full Time</span>
+                <span>USA only</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Frontend</span>
+            <span className="level">Senior</span>
+            <span className="languages">HTML</span>
+            <span className="languages">CSS</span>
+            <span className="languages">JavaScript</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/manage.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>Manage</span>
+                <div className="status">
+                  <span className="new">New!</span>
+                  <span className="featured">Featured</span>
+                </div>
+              </div>
+              <div>
+                <h2>Fullstack Developer</h2>
+              </div>
+              <div className="info">
+                <span>1d ago</span>
+                <span>Part Time</span>
+                <span>Remote</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Fullstack</span>
+            <span className="level">Midweight</span>
+            <span className="languages">Python</span>
+            <span className="tools">React</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/account.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>Account</span>
+                <div className="status">
+                  <span className="new">New!</span>
+                </div>
+              </div>
+              <div>
+                <h2>Junior Frontend Developer</h2>
+              </div>
+              <div className="info">
+                <span>2d ago</span>
+                <span>Part Time</span>
+                <span>USA only</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Frontend</span>
+            <span className="level">Junior</span>
+            <span className="tools">React</span>
+            <span className="tools">Sass</span>
+            <span className="languages">JavaScript</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/myhome.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>MyHome</span>
+              </div>
+              <div>
+                <h2>Junior Frontend Developer</h2>
+              </div>
+              <div className="info">
+                <span>5d ago</span>
+                <span>Contract</span>
+                <span>USA only</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Frontend</span>
+            <span className="level">Junior</span>
+            <span className="languages">CSS</span>
+            <span className="languages">JavaScript</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/loop-studios.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>Loop Studios</span>
+              </div>
+              <div>
+                <h2>Software Engineer</h2>
+              </div>
+              <div className="info">
+                <span>1w ago</span>
+                <span>Full Time</span>
+                <span>Worldwide</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Fullstack</span>
+            <span className="level">Midweight</span>
+            <span className="languages">JavaScript</span>
+            <span className="languages">Ruby</span>
+            <span className="tools">Sass</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/faceit.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>FaceIt</span>
+              </div>
+              <div>
+                <h2>Junior Backend Developer</h2>
+              </div>
+              <div className="info">
+                <span>2w ago</span>
+                <span>Full Time</span>
+                <span>UK only</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Backend</span>
+            <span className="level">Junior</span>
+            <span className="languages">Ruby</span>
+            <span className="tools">RoR</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/shortly.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>Shortly</span>
+              </div>
+              <div>
+                <h2>Junior Developer</h2>
+              </div>
+              <div className="info">
+                <span>2w ago</span>
+                <span>Full Time</span>
+                <span>Worldwide</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Frontend</span>
+            <span className="level">Junior</span>
+            <span className="languages">HTML</span>
+            <span className="languages">JavaScript</span>
+            <span className="tools">Sass</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/insure.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>Insure</span>
+              </div>
+              <div>
+                <h2>Junior Frontend Developer</h2>
+              </div>
+              <div className="info">
+                <span>2w ago</span>
+                <span>Full Time</span>
+                <span>USA only</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Frontend</span>
+            <span className="level">Junior</span>
+            <span className="languages">JavaScript</span>
+            <span className="tools">Vue</span>
+            <span className="tools">Sass</span>
+          </div>
+        </article>
+        <article className="card">
+          <div className="card-left">
+            <img className="card-img" src="/images/eyecam-co.svg" alt="" />
+            <div className="card-top">
+              <div className="card-header">
+                <span>Eyecam Co.</span>
+              </div>
+              <div>
+                <h2>Full Stack Engineer</h2>
+              </div>
+              <div className="info">
+                <span>3w ago</span>
+                <span>Full Time</span>
+                <span>Worldwide</span>
+              </div>
+            </div>
+          </div>
+          <div className="card_right">
+            <span className="role">Fullstack</span>
+            <span className="level">Midweight</span>
+            <span className="languages">JavaScript</span>
+            <span className="languages">Python</span>
+            <span className="tools">Django</span>
+          </div>
+        </article>
+        <div className="attribution">
+          Challenge by{' '}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </main>
     </>
   )
 }
